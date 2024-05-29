27436d0c01e392923d37b330852b02e4924df088
Sevo <betont.online@googlemail.com>
2024-05-29 02:39:05 +0200

    update

--- user_todos/settings.py
+++ user_todos/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
-from django.utils.translation import gettext_lazy as _
+# from django.utils.translation import gettext_lazy as _
 
 
 
